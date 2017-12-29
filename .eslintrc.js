ebcf4ecc2284ccebabc6c728d03ffecf4f3abad6
wangjianfei <444128852@qq.com>
2017-12-29 11:45:16 +0800

    add header,footer,eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,41 @@
 module.exports = {
+  //此项是用来告诉eslint找当前配置文件不能往父级查找
   root: true,
-  parser: 'babel-eslint',
+  //此项是用来指定eslint解析器的，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
+  parser: "babel-eslint",
+  // /此项指定环境的全局变量，下面的配置指定为浏览器环境
   env: {
-    browser: true,
-    node: true
+      browser: true,
+      node: true,
+      es6: true
   },
-  extends: 'standard',
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // 此项是用来配置标准的js风格，就是说写代码的时候要规范的写，如果你使用vs-code我觉得应该可以避免出错
+  extends: [
+      'standard'
+  ],
   // required to lint *.vue files
+  // 此项是用来提供插件的，插件名称省略了eslint-plugin-，下面这个配置是用来规范html的
   plugins: [
-    'html'
+      'html'
   ],
   // add your custom rules here
-  rules: {},
+  // 下面这些rules是用来设置从插件来的规范代码的规则，使用必须去掉前缀eslint-plugin-
+  // 主要有如下的设置规则，可以设置字符串也可以设置数字，两者效果一致
+  // "off" -> 0 关闭规则
+  // "warn" -> 1 开启警告规则
+  //"error" -> 2 开启错误规则
+  // 了解了上面这些，下面这些代码相信也看的明白了
+  rules: {
+    // 指定数组的元素之间要以空格隔开(, 后面)， never参数：[ 之前和 ] 之后不能带空格，always参数：[ 之前和 ] 之后必须带空格
+    "array-bracket-spacing": [2,"never"],
+    // 禁止或强制在单行代码块中使用空格(禁用)
+    "block-spacing":[1,"never"],
+    //强制使用一致的缩进 第二个参数为 "tab" 时，会使用tab，
+    // if while function 后面的{必须与if在同一行，java风格。
+    "brace-style": [2,"1tbs", {"allowSingleLine":true}],
+    "indent": ["error", 4],
+    "no-tabs":0
+  },
   globals: {}
 }
---
+++ components/Commonfooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="footer">
+        <h3>{{msg}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            msg: '这里是footer'
+        }
+    }
+}
+</script>
+<style>
+
+</style>
